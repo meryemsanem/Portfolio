6c8114628d9ce619a705936e6cd2aee0bce65be4
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-05-24 09:55:30 +0300

    Updated file.

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }));
-
 const projectsArr = [
   {
     projectName: 'Keeping track of hundreds  of components website',
